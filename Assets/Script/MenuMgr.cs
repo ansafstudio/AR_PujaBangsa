b8d2c00670ab2742f907b75edc352a4d405bc34e
Muhammad Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-08-16 15:49:29 +0700

    Prj

---
+++ Assets/Script/MenuMgr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMgr : MonoBehaviour {
+	public GameObject mainMenu;
+	public GameObject aboutMenu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) // cek saat ada input back key
+		{
+			if (mainMenu.activeSelf) {
+				Application.Quit (); // unutk keluar dari aplikasi saat menu utama ditampilkan
+			} else {
+				mainMenu.SetActive (true); //untuk kembali mengaktifkan menu utama
+			}
+		}
+	}
+
+	public void StartAR() //function saat tap tombol Start
+	{
+		mainMenu.SetActive (false);
+		aboutMenu.SetActive (false);
+	}
+
+	public void OpenAbout() //function saat tap tombol About
+	{
+		aboutMenu.SetActive (true);
+		mainMenu.SetActive (false);
+	}
+
+	public void ExitFromAbout() //function saat tap tombol Exit di menu About
+	{
+		mainMenu.SetActive (true);
+		aboutMenu.SetActive (false);
+	}
+
+	public void PlayVideo() //function saat tap tombol Video
+    {
+        Handheld.PlayFullScreenMovie("Ngurah-Rai-Movie-Trailer.mp4");
+    }
+}
---
+++ Assets/Script/RotationManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour {
+    public GameObject[] Models;
+
+    bool isPressedRightRot;
+    bool isPressedLeftRot;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i < Models.Length; i++)
+        {
+            //Rotation
+            if (isPressedRightRot)
+            {
+				Models[i].transform.Rotate(Vector3.up * Time.deltaTime * 100); //untuk merotate object ke kanan
+            }
+            else if (isPressedLeftRot)
+            {
+				Models[i].transform.Rotate(Vector3.down * Time.deltaTime * 100); // untuk merotateobject ke kiri
+            }
+        }
+    }
+
+	public void RightPressedRot() //function saat menekan tombol Rotasi kanan
+    {
+        isPressedRightRot = true;
+    }
+	public void RightReleasedRot() //function saat melepas tombol Rotasi kanan
+    {
+        isPressedRightRot = false;
+    }
+
+	public void LeftPressedRot() //function saat menekan tombol Rotasi kiri
+    {
+        isPressedLeftRot = true;
+    }
+	public void LeftReleasedRot() //function saat melepas tombol Rotasi kiri
+    {
+        isPressedLeftRot = false;
+    }
+}
